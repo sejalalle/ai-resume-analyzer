007619da40fd7b861f25b2196b4286f892e6edef
sejalalle <allesejal@gmail.com>
2026-06-20 15:47:06 +0530

    Fix API URL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://ai-resume-analyzer-1-m292.onrender.com/api"
 });
 
 export default API;
